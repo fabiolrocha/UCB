803ac81f0a049d828b8d077c273f7ef04e575a03
Fábio <fabiolrocha2013@gmail.com>
2024-09-20 17:47:00 -0300

    Algumas alterações no desafio 3

--- Faculdade 4° semestre/DesafioAula3/src/br/com/imc/calculo/Atleta.java
+++ Faculdade 4° semestre/DesafioAula3/src/br/com/imc/calculo/Atleta.java
@@ -2,13 +2,16 @@ package br.com.imc.calculo;
 
 public class Atleta extends Pessoa {
     
+    // Atributo
     public String esportePraticado;
 
+    // Construtor
     public Atleta(String nome, double peso, double altura, String esportePraticado) {
         super(nome, peso, altura);
         this.esportePraticado = esportePraticado;
     }
 
+    // Calculo do IMC do atleta
     public double calculoImcAtleta(){
         double imcAtleta = super.calculoImc();
         return imcAtleta * 0.95;
--- Faculdade 4° semestre/DesafioAula3/src/br/com/imc/main/Main.java
+++ Faculdade 4° semestre/DesafioAula3/src/br/com/imc/main/Main.java
@@ -6,8 +6,11 @@ import br.com.imc.calculo.Atleta;
 public class Main {
     public static void main(String[] args) {
         
+        //Instaciando o objeto
         Pessoa objPessoa = new Pessoa("João", 80, 1.80);
 
+
+        //Printando os dados
         System.out.println("Nome: " + objPessoa.getNome());
         System.out.println("Peso: " + objPessoa.getPeso());
         System.out.println("Altura: " + objPessoa.getAltura());
@@ -19,8 +22,11 @@ public class Main {
         System.out.println("");
 
 
+        //Instaciando outro objeto
         Atleta objAtleta = new Atleta("Maria", 60, 1.72, "Volei");
     
+
+        //Printando os outros dados
         System.out.println("Nome: " + objAtleta.getNome());
         System.out.println("Peso: " + objAtleta.getPeso());
         System.out.println("Altura: " + objAtleta.getAltura());
